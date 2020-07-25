be1c24517c75357b00d3f02102c9a23cda9a6163
robmc <robert.mcgui@gmail.com>
2020-07-24 20:41:06 -0700

    Replace babel config w/ jest config and ts-jest

--- src/ctci/__tests__/1.1.test.ts
+++ src/ctci/__tests__/1.1.test.ts
@@ -1,5 +1,7 @@
 import sayHello from '../1.1';
 
 describe('1.1', () => {
-    it('returns if a string has all unique characters');
+    it('returns if a string has all unique characters', () => {
+        expect(sayHello('name')).toBe('Hello, name');
+    });
 });
