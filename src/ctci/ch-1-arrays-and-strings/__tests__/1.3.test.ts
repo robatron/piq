645b5f21bb50ca3f831633d2d82f8a978c7847d0
robmc <robert.mcgui@gmail.com>
2020-07-25 21:06:34 -0700

    Impl 1.3

---
+++ src/ctci/ch-1-arrays-and-strings/__tests__/1.3.test.ts
@@ -0,0 +1,27 @@
+import { isPerm } from '../1.3';
+
+const anagram1 = 'anagram';
+const anagram2 = 'nagaram';
+const nonPerms = [
+    // Same length, not an anagram
+    'restful',
+
+    // Different length
+    'Homer Simpson',
+
+    // Same length, all of these letters are in anagram1, but not all anagram1
+    // letters are in here
+    'aannaag',
+];
+
+describe('isPerm', () => {
+    it('returns true if one string is a permutation of another', () => {
+        expect(isPerm(anagram1, anagram2)).toBe(true);
+    });
+
+    it('returns false if one string is not a permutation of another', () => {
+        nonPerms.forEach((np) => {
+            expect(isPerm(anagram1, np)).toBe(false);
+        });
+    });
+});
