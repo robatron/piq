35cbeb47154c59ee453c1312a81d79005b0c77ea
robmc <robert.mcgui@gmail.com>
2020-07-26 17:04:53 -0700

    Add a couple Microsoft questions

---
+++ src/microsoft/largestComponent.ts
@@ -0,0 +1,11 @@
+/**
+ * Given a grid with different colors in a different cell, each color
+ * represented by a different number. The task is to find out the largest
+ * connected component on the grid. Largest component grid refers to a maximum
+ * set of cells such that you can move from any cell to any other cell in this
+ * set by only moving between side-adjacent cells from the set.
+ *
+ * Note: Read that this question was asked in a Microsoft interview. Problem
+ * detailed and outlined here:
+ * https://www.geeksforgeeks.org/largest-connected-component-on-a-grid/
+ */
