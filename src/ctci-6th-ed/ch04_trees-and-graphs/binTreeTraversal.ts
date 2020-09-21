dc18c9a0f26098843d64b3f03e2db4e0af68b9d2
robmc <robert.mcgui@gmail.com>
2020-09-20 19:33:52 -0700

    Add support for strings in BTNode

--- src/ctci-6th-ed/ch04_trees-and-graphs/binTreeTraversal.ts
+++ src/ctci-6th-ed/ch04_trees-and-graphs/binTreeTraversal.ts
@@ -1,16 +1,18 @@
 import BinTreeNode from './BinTreeNode';
 
 /**
- * Binary trees can be traversed in the following orders:
+ * Binary trees can be traversed in the following orders depth-first orders:
  *
  * - In-order (most common)
- * - Pre-order
+ * - Pre-order (directory tree-style)
  * - Post-order
  */
 
 // Traverse a binary tree in-order (left node, current node, right node)
-export const getBinaryTreeNodesInOrder = (BTree: BinTreeNode) => {
-    let nodeValues: number[] = [];
+export const getBinaryTreeNodesInOrder = (
+    BTree: BinTreeNode,
+): (number | string)[] => {
+    let nodeValues: (number | string)[] = [];
     if (BTree !== null) {
         nodeValues = nodeValues.concat(
             getBinaryTreeNodesInOrder(BTree.leftNode),
@@ -24,8 +26,10 @@ export const getBinaryTreeNodesInOrder = (BTree: BinTreeNode) => {
 };
 
 // Traverse a binary tree pre-order (current node, left node, right node)
-export const getBinaryTreeNodesPreOrder = (BTree: BinTreeNode) => {
-    let nodeValues: number[] = [];
+export const getBinaryTreeNodesPreOrder = (
+    BTree: BinTreeNode,
+): (number | string)[] => {
+    let nodeValues: (number | string)[] = [];
     if (BTree !== null) {
         nodeValues.push(BTree.value);
         nodeValues = nodeValues.concat(
@@ -38,9 +42,14 @@ export const getBinaryTreeNodesPreOrder = (BTree: BinTreeNode) => {
     return nodeValues;
 };
 
+//
+//export const getBinaryTreeNodesPreOrderIterative(BTree: BinTreeNode)
+
 // Traverse a binary tree post-order (left node, right node, current node)
-export const getBinaryTreeNodesPostOrder = (BTree: BinTreeNode) => {
-    let nodeValues: number[] = [];
+export const getBinaryTreeNodesPostOrder = (
+    BTree: BinTreeNode,
+): (number | string)[] => {
+    let nodeValues: (number | string)[] = [];
     if (BTree !== null) {
         nodeValues = nodeValues.concat(
             getBinaryTreeNodesPostOrder(BTree.leftNode),
