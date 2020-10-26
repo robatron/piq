451b72c3a2b31460fde0bb066962b62c8234b01d
robmc <robert.mcgui@gmail.com>
2020-10-25 21:47:22 -0700

    Update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,20 @@
 module.exports = {
-    plugins: ['jest', '@typescript-eslint', 'prettier'],
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    parserOptions: {
-        ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
+    env: {
+        'jest/globals': true,
+        es2020: true,
+        node: true,
     },
     extends: [
         'eslint:recommended',
-
-        // Adds recommended jest rules
-        'plugin:jest/recommended',
-
-        // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'plugin:@typescript-eslint/recommended',
-
-        // Uses eslint-config-prettier to disable ESLint rules from
-        // @typescript-eslint/eslint-plugin that would conflict with prettier
-        'prettier/@typescript-eslint',
-
-        // Enables eslint-plugin-prettier and eslint-config-prettier. This will
-        // display prettier errors as ESLint errors. Make sure this is always
-        // the last configuration in the extends array.
-        'plugin:prettier/recommended',
+        'plugin:jest/recommended',
+        'prettier',
     ],
-    rules: {
-        // disable the rule for all files
-        '@typescript-eslint/explicit-function-return-type': 'error',
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
     },
+    plugins: ['@typescript-eslint', 'jest'],
+    rules: {},
 };
