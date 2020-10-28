cc5b5ef0000d73ecd5b028a62602244fbc9bd1ea
robmc <robert.mcgui@gmail.com>
2020-10-27 20:00:22 -0700

    Create GraphNode class and initial swing at 4.1

--- src/ctci-6th-ed/ch04_trees-and-graphs/4.1_routeBetweenNodes.ts
+++ src/ctci-6th-ed/ch04_trees-and-graphs/4.1_routeBetweenNodes.ts
@@ -1,6 +1,41 @@
+import { GraphNode } from './GraphNode';
+
 /**
  * 4.1 - Route between nodes
  *
- * Given a directed graph, design an algorithm to find out wether there is a
+ * Given a directed graph, design an algorithm to find out whether there is a
  * route between two nodes.
+ *
+ * @param A First node
+ * @param B Second node
  */
+export const isRouteBetweenNodes = (
+    A: GraphNode,
+    B: GraphNode,
+    isReverseDirection = false,
+): boolean => {
+    const searchQueue: GraphNode[] = [A];
+
+    // Breadth-first search from A -> B
+    while (searchQueue.length) {
+        const curNode = searchQueue.pop();
+
+        if (!curNode.visited) {
+            // If we find B, there is a route between A and B
+            if (curNode === B) {
+                return true;
+            }
+
+            // Otherwise, continue the BFS
+            searchQueue.unshift.apply(curNode.neighbors);
+
+            curNode.visited = true;
+        }
+    }
+
+    // If we didn't find a route from A -> B, look for a route from B -> A
+    if (!isReverseDirection) {
+        return isRouteBetweenNodes(B, A, true);
+    }
+    false;
+};
---
+++ src/ctci-6th-ed/ch04_trees-and-graphs/GraphNode.ts
@@ -0,0 +1,10 @@
+export class GraphNode<T = number> {
+    value: T;
+    neighbors: GraphNode[];
+    visited = false;
+
+    constructor(value: T, neighbors: GraphNode[]) {
+        this.value = value;
+        this.neighbors = neighbors;
+    }
+}
