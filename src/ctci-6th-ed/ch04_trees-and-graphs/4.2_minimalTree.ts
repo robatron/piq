bcf583bd9fa1c5a313ada892527d0c8a21fb7c95
robmc <robert.mcgui@gmail.com>
2020-11-01 11:05:18 -0800

    Placeholder

---
+++ src/ctci-6th-ed/ch04_trees-and-graphs/4.2_minimalTree.ts
@@ -0,0 +1,7 @@
+/**
+ * 4.2 - Minimal Tree
+ *
+ * Given a sorted (increasing order) array with unique integer elements, write
+ * an algorithm to create a binary search tree with minimal height.
+ */
+export const minimalTree = (binTreeArray: []) => {};
