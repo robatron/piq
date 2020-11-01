43ebdc4ab982853f4cc8bac6a419442132c0ffa8
robmc <robert.mcgui@gmail.com>
2020-11-01 11:55:43 -0800

    WIP: 4.2

--- src/ctci-6th-ed/ch04_trees-and-graphs/4.2_minimalTree.ts
+++ src/ctci-6th-ed/ch04_trees-and-graphs/4.2_minimalTree.ts
@@ -1,7 +1,60 @@
+import BinTreeNode, { cn } from './BinTreeNode';
+
 /**
  * 4.2 - Minimal Tree
  *
  * Given a sorted (increasing order) array with unique integer elements, write
  * an algorithm to create a binary search tree with minimal height.
  */
-export const minimalTree = (binTreeArray: []) => {};
+export const minimalTree = (
+    binTreeArray: number[],
+    rootIndex = 0,
+): BinTreeNode => {
+    const rootVal = binTreeArray[0];
+    let leftChild = null;
+    let rightChild = null;
+
+    // Base case: return immediately if no root value
+    if (rootVal === null || rootVal === undefined) {
+        return;
+    }
+
+    // If there's a left child value, create a left node
+    if (binTreeArray[1]) {
+        const leftChildIdx = 1;
+        const leftChildLeftIdx = 2 * leftChildIdx + 1;
+        const leftChildRightIdx = 2 * leftChildIdx + 2;
+
+        leftChild = cn(binTreeArray[leftChildIdx]);
+
+        // If there's a left grandchild value, recursively call minimalTree
+        // with it as the root
+        if (binTreeArray[leftChildLeftIdx]) {
+            leftChild.setLeftChild(
+                minimalTree(binTreeArray.slice(leftChildIdx)),
+            );
+        }
+
+        // If there's a right grandchild value, recursively call minimalTree
+        // with it as the root
+        if (binTreeArray[leftChildRightIdx]) {
+            leftChild.setRightChild(
+                minimalTree(binTreeArray.slice(leftChildRightIdx)),
+            );
+        }
+    }
+
+    if (binTreeArray[2]) {
+        rightChild = cn(binTreeArray[2]);
+
+        if (binTreeArray[5]) {
+            leftChild.setLeftChild(minimalTree(binTreeArray.slice(5)));
+        }
+
+        if (binTreeArray[6]) {
+            leftChild.setRightChild(minimalTree(binTreeArray.slice(6)));
+        }
+    }
+
+    return cn(rootVal, leftChild, rightChild);
+};
---
+++ src/ctci-6th-ed/ch04_trees-and-graphs/__tests__/4.2_minimalTree.test.ts
@@ -0,0 +1,13 @@
+import { minimalTree } from '../4.2_minimalTree';
+import { getBinTreeDisplayLines } from '../binTreeDisplay';
+
+describe(minimalTree, () => {
+    it('constructs a binary search tree of minimal height from an array with unique, increasing integers', () => {
+        // Create a 15-length array with elements 0-14
+        const treeArr = [...Array(15).keys()];
+        const actual = minimalTree(treeArr);
+
+        const actualDisplay = getBinTreeDisplayLines(actual);
+        console.log('🚀: actualDisplay', actualDisplay);
+    });
+});
