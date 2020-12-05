fbae5985f781ca6769344dc48695f05e8890cf17
robmc <robert.mcgui@gmail.com>
2020-12-05 10:56:45 -0800

    Basic functionality working

--- src/ctci-6th-ed/ch04_trees-and-graphs/__tests__/4.2_minimalTree.test.ts
+++ src/ctci-6th-ed/ch04_trees-and-graphs/__tests__/4.2_minimalTree.test.ts
@@ -1,11 +1,37 @@
 import { minimalTree } from '../4.2_minimalTree';
 import { getBinTreeDisplayLines } from '../binTreeDisplay';
+import { cn } from '../BinTreeNode';
 
 describe(minimalTree, () => {
     it('constructs a binary search tree of minimal height from an array with unique, increasing integers', () => {
         // Create a 15-length array with elements 0-14
         const treeArr = [...Array(15).keys()];
         const actual = minimalTree(treeArr);
+
+        // prettier-ignore
+        const expected = cn(
+            0,
+            cn(1,
+                cn(3,
+                    cn(7),
+                    cn(8)
+                ),
+                cn(4,
+                    cn(9),
+                    cn(10)
+                )
+            ),
+            cn(2,
+                cn(5,
+                    cn(11),
+                    cn(12)
+                ),
+                cn(6,
+                    cn(13),
+                    cn(14)
+                )
+            ),
+        );
 
         const actualDisplay = getBinTreeDisplayLines(actual, {
             showLeftRightLabel: true,
@@ -14,5 +40,7 @@ describe(minimalTree, () => {
             '🚀: actualDisplay',
             JSON.stringify(actualDisplay, null, 2),
         );
+
+        expect(actual).toStrictEqual(expected);
     });
 });
