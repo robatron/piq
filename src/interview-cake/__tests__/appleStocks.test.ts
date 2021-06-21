af9271eb5b8035018a256f2a8f55d6e3a6cc67b0
Rob McGuire <robert.mcgui@gmail.com>
2021-06-20 18:25:20 -0700

    IC: Apple stocks

--- src/interview-cake/__tests__/appleStocks.test.ts
+++ src/interview-cake/__tests__/appleStocks.test.ts
@@ -1,16 +1,56 @@
-import { getMaxProfit } from '../appleStocks';
+import appleStocks, { appleStocksNaive } from '../appleStocks';
 
-describe('getMaxProfit', () => {
+describe('appleStocksNaive', () => {
     it('returns the best profit one could have made from one purchase and one sale of one share of Apple stock yesterday', () => {
-        expect(getMaxProfit([10, 7, 5, 8, 11, 9])).toBe(6);
+        const stockPrices = [10, 7, 5, 8, 11, 9];
+        const expected = 6;
+
+        expect(appleStocksNaive(stockPrices)).toBe(expected);
+        expect(appleStocks(stockPrices)).toBe(expected);
     });
 
     it('enforces buying before selling', () => {
-        expect(getMaxProfit([10, 7, 11, 8, 5, 9])).toBe(4);
+        const stockPrices = [10, 7, 11, 8, 5, 9];
+        const expected = 4;
+
+        expect(appleStocksNaive(stockPrices)).toBe(expected);
+        expect(appleStocks(stockPrices)).toBe(expected);
     });
 
     it('handles negative max profit', () => {
         // -1 smallest amount you can lose
-        expect(getMaxProfit([11, 10, 9, 8, 7, 5])).toBe(-1);
+        const stockPrices = [11, 10, 9, 8, 7, 5];
+        const expected = -1;
+
+        expect(appleStocksNaive(stockPrices)).toBe(expected);
+        expect(appleStocks(stockPrices)).toBe(expected);
+    });
+
+    it('handles 0 profit', () => {
+        const stockPrices = [3, 3, 3];
+        const expected = 0;
+
+        expect(appleStocksNaive(stockPrices)).toBe(expected);
+        expect(appleStocks(stockPrices)).toBe(expected);
+    });
+
+    it('handles constant price drop', () => {
+        const stockPrices = [10, 5, 3, 1, -1];
+        const expected = -2;
+
+        expect(appleStocksNaive(stockPrices)).toBe(expected);
+        expect(appleStocks(stockPrices)).toBe(expected);
+    });
+
+    it('throws when time window too small', () => {
+        const stockPrices = [3];
+
+        expect(() => {
+            appleStocksNaive(stockPrices);
+        }).toThrow();
+
+        expect(() => {
+            appleStocks(stockPrices);
+        }).toThrow();
     });
 });
