a5c5c44263901544458b0a631c8e8dc7319ca86e
Rob McGuire <robert.mcgui@gmail.com>
2021-11-14 14:31:24 -0800

    LC: 975. Odd even jump

---
+++ src/leetcode/__tests__/975.odd-even-jump.test.ts
@@ -0,0 +1,13 @@
+import { oddEvenJumps, oddEvenJumpsDP } from '../975.odd-even-jump';
+import { createTests } from './utils';
+
+const ios: [number[], number][] = [
+    [[5], 1],
+    [[10, 13, 12, 14, 15], 2],
+    [[2, 3, 1, 1, 4], 3],
+    [[5, 1, 3, 4, 2], 3],
+];
+
+describe('oddEvenJumps', () => {
+    createTests(ios, oddEvenJumps);
+});
