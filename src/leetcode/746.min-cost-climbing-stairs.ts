15739231a7476a8eaa4898ad652a721be87763f4
Rob McGuire <robert.mcgui@gmail.com>
2021-11-20 11:47:07 -0800

    LC: 746: Min cost climbing stairs (revision)

--- src/leetcode/746.min-cost-climbing-stairs.ts
+++ src/leetcode/746.min-cost-climbing-stairs.ts
@@ -7,8 +7,8 @@
  *
  * Easy (55.65%)
  *
- * You are given an integer array `cost` where `cost[i]` is the cost of `i`th step
- * on a staircase. Once you pay the cost, you can either climb one or two
+ * You are given an integer array `cost` where `cost[i]` is the cost of `ith`
+ * step on a staircase. Once you pay the cost, you can either climb one or two
  * steps.
  *
  * You can either start from the step with index 0, or the step with index 1.
@@ -19,23 +19,23 @@
  *
  * - Input: cost = [10,15,20]
  * - Output: 15
- * - Explanation: You will start at index 1
+ * - Explanation:
+ *   - Start at index 1
  *   - Pay 15
- *   - Climb two steps to reach the top
- *   => The total cost is 15
+ *   - Climb two steps to reach the top => The total cost is 15
  *
  * Example 2:
  *
  * - Input: cost = [1,100,1,1,1,100,1,1,100,1]
  * - Output: 6
- * - Explanation: You will start at index 0
+ * - Explanation:
+ *   - Start at index 0
  *   - Pay 1 and climb two steps to reach index 2
  *   - Pay 1 and climb two steps to reach index 4
  *   - Pay 1 and climb two steps to reach index 6
  *   - Pay 1 and climb one step to reach index 7
  *   - Pay 1 and climb two steps to reach index 9
- *   - Pay 1 and climb one step to reach the top
- *   => The total cost is 6
+ *   - Pay 1 and climb one step to reach the top => The total cost is 6
  *
  * Constraints:
  *
@@ -44,32 +44,43 @@
  *
  * Notes:
  *
- * [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * - [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
  *
- * idx  cost    prevCosts (1 back | 2 back)
+ * - idx  cost    prevCosts (1 back | 2 back)
  *
- * 0:   1       0
- * 1:   100     0+1                                                 | 0
- * 2:   1       100+0+1, 100+0                                      | 0+1
- * 3:   1       100+0+1+1, 100+0+1, 0+1+1                           | 100+0+1, 100+0
- * 4:   1       100+0+1+1+1, 100+0+1+1, 0+1+1+1, 100+0+1+1, 100+0+1 | 100+0+1+1, 100+0+1, 0+1+1
+ * - 0:   1       0
+ * - 1:   100     0+1                       | 0
+ * - 2:   1       100+0+1, 100+0            | 0+1
+ * - 3:   1       100+0+1+1, 100+0+1, 0+1+1 | 100+0+1, 100+0
  */
 
 // @lc code=start
-const minCostClimbingStairs = (cost: number[]): number => {
-    let minCostBack1 = 0;
-    let minCostBack2 = 0;
 
-    for (let i = 0; i < cost.length; i++) {
-        const curStepCost: number = cost[i];
-        const minPathCost: number = Math.min(
-            minCostBack1 + curStepCost,
-            minCostBack2 + curStepCost,
-        );
+/**
+ * Dynamic programming. "For every stair, is it cheaper to get here from one
+ * stair back, or two stairs back?
+ *
+ * Time:    O(n)    - Consider every stair once
+ * Space:   O(1)    - Only need to store the min cost of one and two stairs back
+ */
+const minCostClimbingStairs = (cost: number[]): number => {
+    // No stairs = no costs, or 1 stair = that stair's cost
+    if (!cost.length) return 0;
+    if (cost.length === 1) return cost[0];
+
+    // Track the minimum cost of getting two stairs back and one stair back.
+    // Initialize to the cost of getting to the 0th stair and the 1st stair
+    let minCostBack2 = cost[0];
+    let minCostBack1 = cost[1];
+
+    for (let stair = 2; stair < cost.length; stair++) {
+        const minBackCost: number = Math.min(minCostBack1, minCostBack2);
         minCostBack2 = minCostBack1;
-        minCostBack1 = minPathCost;
+        minCostBack1 = minBackCost + cost[stair];
     }
 
+    // Finally, now that we're at the top floor, would it cost less to come from
+    // one stair back or two stairs back?
     return Math.min(minCostBack1, minCostBack2);
 };
 // @lc code=end
