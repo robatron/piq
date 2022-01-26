94ef8a0378df86767f3bf936a29a294b695d5118
robmc <robert.mcgui@gmail.com>
2022-01-26 10:25:09 -0800

    TODO: LC: 22. Generate parens

---
+++ src/leetcode/22.generate-parentheses.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=22 lang=typescript
+ *
+ * [22] Generate Parentheses
+ *
+ * https://leetcode.com/problems/generate-parentheses/description/
+ *
+ * algorithms
+ * Medium (68.28%)
+ * Likes:    11366
+ * Dislikes: 443
+ * Total Accepted:    959.1K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '3'
+ *
+ * Given n pairs of parentheses, write a function to generate all combinations
+ * of well-formed parentheses.
+ * 
+ * 
+ * Example 1:
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ * Example 2:
+ * Input: n = 1
+ * Output: ["()"]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 8
+ * 
+ * 
+ */
+
+// @lc code=start
+function generateParenthesis(n: number): string[] {
+
+};
+// @lc code=end
+
